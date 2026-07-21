2467f9a3b38b7efb6f2daf29585cf177c961d99d
naveenpv547 <naveen12rvb2022@gmail.com>
2026-07-21 17:24:38 +0530

    fix: change frontend proxy port to 5002

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -32,15 +32,15 @@ export default defineConfig(() => {
       },
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:5000',
+          target: 'http://127.0.0.1:5002',
           changeOrigin: true,
         },
         '/public': {
-          target: 'http://127.0.0.1:5000',
+          target: 'http://127.0.0.1:5002',
           changeOrigin: true,
         },
         '/uploads': {
-          target: 'http://127.0.0.1:5000',
+          target: 'http://127.0.0.1:5002',
           changeOrigin: true,
         },
       },
